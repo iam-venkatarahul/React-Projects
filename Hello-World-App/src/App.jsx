f4c34e5c096b3f66be5adfd0f370681e075fe696
iam-venkatarahul <chintakayalavvrahul@gmail.com>
2025-07-26 14:25:29 +0530

    Demo React Project

---
+++ Hello-World-App/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='AppComponent'>
+        <h1>Welcome To React Projects...</h1>
+        <h1>This is my First React Project</h1>
+      </div>
+    </>
+  )
+}
+export default App
