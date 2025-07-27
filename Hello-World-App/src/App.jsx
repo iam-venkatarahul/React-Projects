0aae7645c198c9406ff593254b601425b2431def
iam-venkatarahul <chintakayalavvrahul@gmail.com>
2025-07-27 20:37:18 +0530

    Modified

--- Hello-World-App/src/App.jsx
+++ Hello-World-App/src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
 
+function Greeting(isMorning){
+  return isMorning ? <h1>Good Morning User!</h1> : <h1>Good Evening User!</h1>;
+}
+
 export default function App() {
   const buttonText = 'Submit!'
+  const dateobj = new Date()
+  const isMorning = dateobj.getHours() < 12
   return (
       <div className='App'>
+        {Greeting(isMorning)}
         <h1>Welcome To React Projects...</h1>
         <h1>This is my First React Project</h1>
         <button style={{backgroundColor:'orange'}} onClick={() => alert('Submitted!')}>
