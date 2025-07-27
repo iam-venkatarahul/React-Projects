b807c6d1a4f5daaf382b93b163729fcd7a3d3b32
iam-venkatarahul <chintakayalavvrahul@gmail.com>
2025-07-27 20:10:46 +0530

    Updated

--- Hello-World-App/src/App.jsx
+++ Hello-World-App/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 
 export default function App() {
+  const buttonText = 'Submit!'
   return (
     <>
       <div className='App'>
         <h1>Welcome To React Projects...</h1>
         <h1>This is my First React Project</h1>
+        <button style={{backgroundColor:'orange'}} onClick={() => alert('Submitted!')}>
+          {buttonText}</button>
       </div>
     </>
   )
 }
-
