70eeb2cb6faf1a480758560b2cf6bd9750885907
iam-venkatarahul <chintakayalavvrahul@gmail.com>
2025-07-30 22:25:10 +0530

    initial comit

---
+++ Props-Demo/src/StudentProfile.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+
+export default function StudentProfile({name, age, IsDue}) {
+    //Validation of props using PropTypes
+    StudentProfile.propTypes = {
+        name: PropTypes.string.isRequired,      
+        age: PropTypes.number.isRequired,
+        FeesPaid: PropTypes.bool.isRequired 
+    };
+
+    return (
+        <div className="student-profile">
+            <h3>Student Name - {name}</h3>
+            <h3>Student Age - {age}</h3>
+            <h3>FeeIsDue - {IsDue ? "Yes" : "No"}</h3>
+        </div>
+    );
+}
