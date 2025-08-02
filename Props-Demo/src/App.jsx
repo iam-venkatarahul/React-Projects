ad517ffb3c25c999aefbb0ee17c702c0a0b72a96
iam-venkatarahul <chintakayalavvrahul@gmail.com>
2025-08-02 12:44:26 +0530

    updated

--- Props-Demo/src/App.jsx
+++ Props-Demo/src/App.jsx
@@ -3,15 +3,25 @@ import StudentProfile from './StudentProfile'
 
 export default function App() {
 
-  const handleGitClick = (GithubLink) => {
-    alert(`You Clicked on the GitHub link: ${GithubLink}`);
-  }
-    return (
+  const UserDetails1 = {
+    name: "Rahul",
+    age: 21,
+    IsDue: false,
+    GithubLink: true
+  };
+
+  const UserDetails2 = {
+    name: "Shalini",            
+    age: 21,
+    IsDue: true,
+    GithubLink: false
+  };
+   return (
     <>
       <div className='App'>
         <h2>Hello</h2>
-        <StudentProfile name="Rahul" age="21" IsDue= {false} GithubLink={true} />
-        <StudentProfile name="Shalini" age="21" IsDue = {true} GithubLink= {false} />
+        <StudentProfile {...UserDetails1} />
+        <StudentProfile {...UserDetails2} />
       </div>
     </>
   )
