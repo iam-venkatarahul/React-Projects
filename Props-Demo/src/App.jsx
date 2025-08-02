d3b4fa81fe6264c775cba3d4fc821828a45d78b9
iam-venkatarahul <chintakayalavvrahul@gmail.com>
2025-08-02 12:23:35 +0530

    modified

--- Props-Demo/src/App.jsx
+++ Props-Demo/src/App.jsx
@@ -2,13 +2,18 @@ import './App.css'
 import StudentProfile from './StudentProfile'
 
 export default function App() {
+
+  const handleGitClick = (GithubLink) => {
+    alert(`You Clicked on the GitHub link: ${GithubLink}`);
+  }
     return (
     <>
-      <div>
+      <div className='App'>
         <h2>Hello</h2>
-        <StudentProfile name="Rahul" age="21" IsDue= {false} />
-        <StudentProfile name="Shalini" age="21" IsDue = {true}/>
+        <StudentProfile name="Rahul" age="21" IsDue= {false} GithubLink={true} />
+        <StudentProfile name="Shalini" age="21" IsDue = {true} GithubLink= {false} />
       </div>
     </>
   )
 }
+ 
--- Props-Demo/src/StudentProfile.jsx
+++ Props-Demo/src/StudentProfile.jsx
@@ -1,11 +1,14 @@
 import PropTypes from 'prop-types';
 
-export default function StudentProfile({name, age, IsDue}) {
+export default function StudentProfile({name, age, IsDue, GithubLink}) {
+
+    // Default to an empty array if hobbies is not provided
     //Validation of props using PropTypes
     StudentProfile.propTypes = {
         name: PropTypes.string.isRequired,      
         age: PropTypes.number.isRequired,
-        FeesPaid: PropTypes.bool.isRequired 
+        FeesPaid: PropTypes.bool.isRequired,
+        GithubLink: PropTypes.bool.isRequired
     };
 
     return (
@@ -13,6 +16,7 @@ export default function StudentProfile({name, age, IsDue}) {
             <h3>Student Name - {name}</h3>
             <h3>Student Age - {age}</h3>
             <h3>FeeIsDue - {IsDue ? "Yes" : "No"}</h3>
+            <h4>GithubLink - {GithubLink ? "Rahul" : "Shalini"}</h4>
         </div>
     );
 }
